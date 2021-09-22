b35fa6627e20a4a8e6f21755001980a8ed82a69a
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-22 11:30:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ while len(guessed_states) < 50:
         t.hideturtle()
         t.penup()
         state_data = data[data.state == answer_state]
-#         t.goto(int(state_data.x), int(state_data.y))
+        t.goto(int(state_data.x), int(state_data.y))
 #         t.write(answer_state)
 
 
